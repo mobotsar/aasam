999c49cb35fd98e6e4f337e39797a8b1ee21dc1b
Alexander <mobotsar@pm.me>
2022-07-18 15:44:29 -0400

    added functions to flatten production trees into terminal lists, making them easier to work with

--- lib/Aasam.hs
+++ lib/Aasam.hs
@@ -1,7 +1,45 @@
 module Aasam (m) where
 
 import Grammars
-import Data.Set
 
-m :: ContextFree -> Precedence
-m (s, prods) = empty
+import Data.Set
+import qualified Data.Set as Set
+
+data FlatPP =
+      FlatPrefix  Int [Ae]
+    | FlatPostfix Int [Ae]
+    | FlatInfixl  Int [Ae]
+    | FlatInfixr  Int [Ae]
+    | FlatClosed      [Ae]
+type FlatPrecedence = Set.Set FlatPP
+
+renderFlat :: PrecedenceProduction -> FlatPP
+renderFlat (Inl n sub)  = FlatInfixl n (flattenInfixl sub)
+renderFlat (Inr n sub)  = FlatInfixr n (flattenInfixr sub)
+renderFlat (Pre n sub)  = FlatPrefix n (flattenPrefix sub)
+renderFlat (Post n sub) = FlatPostfix n (flattenPostfix sub)
+renderFlat (Closed sub) = FlatClosed (flattenClosed sub)
+
+flattenInfixl :: Infixl -> [Ae]
+flattenInfixl (Infixl sub) = flattenClosed sub
+
+flattenInfixr :: Infixr -> [Ae]
+flattenInfixr (Infixr sub) = flattenClosed sub
+
+flattenPrefix :: Prefix -> [Ae]
+flattenPrefix (Prefix sub) = flattenClosed sub
+
+flattenPostfix :: Postfix -> [Ae]
+flattenPostfix (Postfix sub) = flattenClosed sub
+
+flattenClosed :: Closed -> [Ae]
+flattenClosed (Op sub) = [sub]
+flattenClosed (SubInl ae0 inl ae1) = ae0 : flattenInfixl inl ++ [ae1]
+flattenClosed (SubInr ae0 inr ae1) = ae0 : flattenInfixr inr ++ [ae1]
+flattenClosed (SubPre ae0 pre ae1) = ae0 : flattenPrefix pre ++ [ae1]
+flattenClosed (SubPost ae0 post ae1) = ae0 : flattenPostfix post ++ [ae1]
+flattenClosed (SubClosed ae0 closed ae1) = ae0 : flattenClosed closed ++ [ae1]
+
+m :: Precedence -> ContextFree
+m prec = (NonTerminal "", empty)
+
--- lib/Grammars.hs
+++ lib/Grammars.hs
@@ -13,9 +13,22 @@ newtype Infixl  = Infixl Closed deriving (Eq, Ord)
 newtype Infixr  = Infixr Closed deriving (Eq, Ord)
 newtype Prefix  = Prefix Closed deriving (Eq, Ord)
 newtype Postfix = Postfix Closed deriving (Eq, Ord)
-data Closed     = Op Ae | SubInl Infixl | SubInr Infixr | SubPre Prefix | SubPost Postfix | SubClosed Closed deriving (Eq, Ord)
-type Ae         = String
+data Closed =
+      Op Ae
+    | SubInl Ae Infixl Ae
+    | SubInr Ae Infixr Ae
+    | SubPre Ae Prefix Ae
+    | SubPost Ae Postfix Ae
+    | SubClosed Ae Closed Ae
+    deriving (Eq, Ord)
+type Ae = String
 
-data PrecedenceProduction = Inl (Infixl, Int) | Inr (Infixr, Int) | Pre (Prefix, Int) | Post (Postfix, Int) | Closed Closed deriving (Eq, Ord)
+data PrecedenceProduction =
+      Inl Int Infixl
+    | Inr Int Infixr
+    | Pre Int Prefix
+    | Post Int Postfix
+    | Closed Closed
+    deriving (Eq, Ord)
 
 type Precedence = Set PrecedenceProduction
