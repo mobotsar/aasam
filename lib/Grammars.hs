6757a0fa2c7a6ceb8df5c0870a441d0ef1e3d5f7
Alexander Lucas <mobotsar@protonmail.com>
2022-07-18 16:11:24 +0000

    Update Grammars.hs

--- lib/Grammars.hs
+++ lib/Grammars.hs
@@ -8,10 +8,10 @@ import qualified Data.Set as Set
 newtype Infix = Infix Closed
 newtype Prefix = Prefix Closed
 newtype Postfix = Postfix Closed
-data Closed = Op Op | SubIn Infix | SubPre Prefix | SubPost Postfix | SubClosed Closed
-type Op = String
+data Closed = Op Ae | SubIn Infix | SubPre Prefix | SubPost Postfix | SubClosed Closed
+type Ae = String
 
-data DistfixRhs = In Infix | Pre Prefix | Post Postfix | Closed Closed | Ae Op
+data DistfixRhs = In Infix | Pre Prefix | Post Postfix | Closed Closed
 
 instance Eq DistfixRhs
 instance Ord DistfixRhs 
