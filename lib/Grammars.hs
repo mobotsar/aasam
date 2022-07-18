d82ff5bff6f163891e22dfbe18f7a923db4deab5
Alexander Lucas <mobotsar@protonmail.com>
2022-07-18 17:47:19 +0000

    Update grammar definntions

--- lib/Grammars.hs
+++ lib/Grammars.hs
@@ -1,19 +1,21 @@
-{-# OPTIONS_GHC -Wno-missing-methods #-}
-
 module Grammars where
 
 import Data.Set (Set)
 import qualified Data.Set as Set
 
-newtype Infix = Infix Closed
-newtype Prefix = Prefix Closed
-newtype Postfix = Postfix Closed
-data Closed = Op Ae | SubIn Infix | SubPre Prefix | SubPost Postfix | SubClosed Closed
-type Ae = String
+newtype NonTerminal = NonTerminal String
+newtype Terminal    = Terminal String
+type CfgString      = [Either Terminal NonTerminal]
+type CfgProduction  = (NonTerminal, CfgString)
+type ContextFree    = (NonTerminal, Set CfgProduction)
 
-data DistfixRhs = In Infix | Pre Prefix | Post Postfix | Closed Closed
+newtype Infixl  = Infixl Closed deriving (Eq, Ord)
+newtype Infixr  = Infixr Closed deriving (Eq, Ord)
+newtype Prefix  = Prefix Closed deriving (Eq, Ord)
+newtype Postfix = Postfix Closed deriving (Eq, Ord)
+data Closed     = Op Ae | SubInl Infixl | SubInr Infixr | SubPre Prefix | SubPost Postfix | SubClosed Closed deriving (Eq, Ord)
+type Ae         = String
 
-instance Eq DistfixRhs
-instance Ord DistfixRhs 
+data PrecedenceProduction = Inl (Infixl, Int) | Inr (Infixr, Int) | Pre (Prefix, Int) | Post (Postfix, Int) | Closed Closed deriving (Eq, Ord)
 
-type Distfix = Set DistfixRhs
+type Precedence = Set PrecedenceProduction
